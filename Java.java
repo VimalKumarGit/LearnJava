ceb73c2b23444c8583675eb5235078d92e2bd1a9
VimalKumarGit <vimalblacksmith@gmail.com>
2021-03-21 23:24:53 +0530

    added java file

---
+++ Java.java
@@ -0,0 +1,22 @@
+public class Java
+{
+
+
+public static void main(String[] args)
+{
+Java obj = new Java(10000);
+Java obj1 = new Java(5.11f, "Black"); 
+}
+
+Java(int a)
+{
+System.out.println("My phone price is "+ a);
+}
+
+Java(float a, String color)
+{
+System.out.println("My height is " + a);
+System.out.println("My skin color is " + color);
+}
+
+}
