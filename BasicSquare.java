07f3df90715ff25c0d596207e882145677d392f7
VimalKumarGit <vimalblacksmith@gmail.com>
2021-04-24 22:53:33 +0530

    added new file

---
+++ BasicSquare.java
@@ -0,0 +1,21 @@
+package starPattern;
+
+public class BasicSquare {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n=5;
+		for(int row=1; row<=n; row++)
+		{
+			for(int col=1; col<=n; col++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+			
+		}
+
+	}
+
+}
